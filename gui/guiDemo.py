876b56376977ba4c272916d4edde94b41aa81155
ykimura517 <y.kimura199412@gmail.com>
2020-02-21 16:22:30 +0900

    gui

--- gui/guiDemo.py
+++ gui/guiDemo.py
@@ -5,18 +5,24 @@ from sasikae import sasikae
 from PIL import ImageTk
 from guiSupport import guiSuppoter,DBManager
 from tkinter import filedialog
+import subprocess
+import tkinter.font as tkFont
 
 detecter = guiSuppoter()
 DBManager=DBManager()
 
+def changeTxt(label,message,bgcolor='#ffffff',fontcolor="#111111"):
+    label["text"] =message
+    label['bg']=bgcolor
+    label['fg']=fontcolor
 
 def draw_img(event):
     global img
     img=ImageTk.PhotoImage(file="po.png")
     event.widget.itemconfig("imageDisplay",image=img,anchor=tk.NW)
 
-def changeImg(panel):
-    img=ImageTk.PhotoImage(file="result.png")
+def changeImg(panel,file="result.png"):
+    img=ImageTk.PhotoImage(file=file)
     panel.configure(image=img)
     panel.image=img
 
@@ -25,31 +31,32 @@ def getFilename(event=None):
     print('Selected:', filename)
     return filename
 
-def mainProcess(panel):
+def mainProcess(panel,panel2,resultPlaceLabel,yomitorinum1Label,yomitorinum2Label):
     targetImage=getFilename()
     # num1,num2=detecter.imgDetect(target_img)
     num1,num2=detecter.imgDetect(targetImage)
     changeImg(panel)
-
-    label3 = tk.Label(root, text="読み取ったナンバー")
-    label3.grid(row=12, column=0, columnspan=1, padx=1, pady=1)
+    changeImg(panel2,targetImage)
 
     if DBManager.isInDb(num1,num2):
         print("OK")
-        label2 = tk.Label(root, text="登録確認車両です")
-        label2.grid(row=10, column=0, columnspan=1, padx=1, pady=1)
-        label4 = tk.Label(root, text=str(num1))
-        label4.grid(row=14, column=0, columnspan=1, padx=1, pady=1)
-        label5 = tk.Label(root, text=str(num2))
-        label5.grid(row=15, column=0, columnspan=1, padx=1, pady=1)
+        changeTxt(resultPlaceLabel,message="登録されている車両です",bgcolor='#2c6ebd',fontcolor="#ffffff")
+        changeTxt(yomitorinum1Label,message=str(num1))
+        changeTxt(yomitorinum2Label,message=str(num2))
+
     else:
         print("NG")
-        label2 = tk.Label(root, text="登録されていない車両の可能性があります")
-        label2.grid(row=10, column=0, columnspan=1, padx=1, pady=1)
+        changeTxt(resultPlaceLabel,message="未登録車両の可能性があります",bgcolor='#ff0000',fontcolor="#f0e68c")
+        changeTxt(yomitorinum1Label,message=str(num1))
+        changeTxt(yomitorinum2Label,message=str(num2))
     #表示する
     return num1,num2
+def openDB():
+    cmd = "libreoffice db.csv"
+    subprocess.call(cmd.split())
 
 
+#bbeebb
 
 
 
@@ -60,46 +67,50 @@ def mainProcess(panel):
 
 #rootウィンドウを作成
 root = tk.Tk()
+root.configure(bg='#bbeebb')
 #rootウィンドウのタイトルを変える
 root.title("ナンバープレート検知デモ")
 #rootウィンドウの大きさを320x240に
-root.geometry("800x800")
+root.geometry("635x500")
 
-#Label部品を作る
-label = tk.Label(root, text="ナンバープレート検知デモ")
-#表示する
-label.grid(row=0, column=0, columnspan=1, padx=1, pady=1)
-
-
-
-img = tk.PhotoImage(file="son.png")
-panel = tk.Label(root,image=img,width = 280, height = 260)
-
+#where result img will be displayed.
+img = tk.PhotoImage(file="dummy.png")
+panel = tk.Label(root,image=img,width = 280, height = 260,relief="groove",bd=3)
 panel.img=img
-# panel.pack(side="top",fill="both",expand="yes")
-panel.grid(row=1, column=3, columnspan=2, padx=15, pady=5)
+panel.grid(row=0, column=3, columnspan=2, padx=15, pady=5)
 
-#ボタンを作る
-# button = tk.Button(root, text="ボタン", command= lambda : pushed(button))
-# button = tk.Button(root, text="ボタン", command=lambda:changeImg(panel))
-# resultLabel = tk.Label(root, text="")
+#This exists mainly to adjust its layoutself.
 
-button = tk.Button(root, text="画像を選択", command=lambda:mainProcess(panel))
+img2 = tk.PhotoImage(file="dummy.png")
+panel2 = tk.Label(root,image=img,width = 280, height = 260,relief="groove",bd=3)
+panel2.img=img
+panel2.grid(row=0, column=1, columnspan=2, padx=15, pady=5)
+
+
+#btn by which we can choose img and detect num
+button = tk.Button(root, text="画像を選択", command=lambda:mainProcess(panel,panel2,resultPlaceLabel,yomitorinum1Label,yomitorinum2Label))
 #表示
-button.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+button.grid(row=1, column=1, columnspan=2, padx=5, pady=5)
+
+button4db = tk.Button(root, text="DBを確認（Ubuntuのみ使用可能）", command=lambda:openDB())
+#表示
+button4db.grid(row=2, column=1, columnspan=2, padx=5, pady=5)
 
 
-# button4choosefile = tk.Button(root, text='画像を選択', command=UploadAction)
-# button4choosefile.grid()
 
-#displaying image
-# img = tk.PhotoImage(file="son.png")
-# canvas=tk.Canvas(bg="white",width=2000,height=2000)
-# canvas.create_image(0,0, image=img,anchor=tk.NW,tag="imageDisplay")
-# canvas.place(x=200,y=50)
+resultrow=1
+resultPlaceLabel = tk.Label(root, text="")
+resultPlaceLabel.grid(row=resultrow, column=4, columnspan=1, padx=1, pady=1)
 
-# canvas.bind('<1>',draw_img)
+label3 = tk.Label(root, text="読み取ったナンバー↓",relief="groove",bd=3)
+label3.grid(row=resultrow+1, column=4, columnspan=1, padx=1, pady=1)
 
+fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+
+yomitorinum1Label = tk.Label(root, text="",background='#bbeebb',font=fontStyle)
+yomitorinum1Label.grid(row=resultrow+2, column=4, columnspan=1, padx=1, pady=1)
+yomitorinum2Label = tk.Label(root, text="",background='#bbeebb',font=fontStyle)
+yomitorinum2Label.grid(row=resultrow+3, column=4, columnspan=1, padx=1, pady=1)
 
 
 #メインループ
